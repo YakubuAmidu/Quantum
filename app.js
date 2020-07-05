534b77df93da5965f0e6d4814a333255309f44a9
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-05 01:07:07 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -1,103 +1,29 @@
 
+// BUDGET CONTROLLER
+var budgetController = (function(){
 
+   var Expense = function(id, description, value) {
+       this.id = id;
+       this.description = description;
+       this.value = value;
+       this.percentage = -1;
+   };
 
-    var Income = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
+     Expense.prototype.calcPercentage = function(totalIncome) {
+
+        if(totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
     };
 
-
-    var calculateTotal = function(type) {
-        var sum = 0;
-        data.allItems[type].forEach(function(cur) {
-           sum += cur.value;
-        });
-        data.totals[type] = sum;
-    };
-
-    var data = {
-        allItems: {
-            exp: [],
-            inc: []
-        },
-        totals: {
-            exp: 0,
-            inc: 0
-        },
-        budget: 0,
-        percentage: -1
-    };
-
-    return {
-        addItem: function(type, des, val) {
-            var newItem, ID;
-
-            // Create new ID
-            if (data.allItems[type].length > 0) {
-
-                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-
-            } else {
-                ID = 0;
-            }
-
-            // Create new Item based on 'inc' or 'exp'
-            if(type === 'exp'){
-                 newItem = new Expense(ID, des, val);
-            } else if (type === 'inc'){
-                newItem = new Income(ID, des, val);
-            }
-
-            // Push it into our data structure
-            data.allItems[type].push(newItem);
-
-            // Return the new element
-            return newItem;
-        },
-
-        deleteItem: function(type, id) {
-            var ids, index;
-
-            // id = 6
-            // data.allItems[type][id]
-            // ids = [1 2 4 6 8]
-            // index = 3
-
-            ids = data.allItems[type].map(function(current) {
-                return current.id;
-            });
-
-            index = ids.indexOf(id);
-
-            if(index !== -1){
-                data.allItems[type].splice(index, 1);
-            }
-        },
-
-        calculateBudget: function() {
-
-            // Calculate total income and expenses
-            calculateTotal('exp');
-            calculateTotal('inc');
-
-            // Calculate the budget: income - expenses
-            data.budget = data.totals.inc - data.totals.exp;
+     Expense.prototype.getPercentage = function() {
+        return this.percentage;
+};
 
 
-            // Calculate the percentage of that we spent
-
-            if(data.totals.inc > 0){
-
-             data.percentage = Math.round((data.totals.exp / data.totals.inc)  * 100);
-
-            }  else {
-                data.percentage = - 1;
-            }
-            // Expense = 100 and income 300, spent 33.333% = 100/300 = 0.333 * 100
-
-
-        },
+  
 
         calculatePercentages: function() {
 
